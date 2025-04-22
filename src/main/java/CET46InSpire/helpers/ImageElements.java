7a6ebc6a9b290ab821a009c063761cc7c24b64e2
sleepyHolo <1665814195@qq.com>
2025-04-22 16:01:28 +0800

    增加n1~n4图像,missing图像

--- src/main/java/CET46InSpire/helpers/ImageElements.java
+++ src/main/java/CET46InSpire/helpers/ImageElements.java
@@ -25,9 +25,14 @@ public class ImageElements {
      * 游戏内元素的图片
      * 遗物 & 能力
      */
+    public static Texture RELIC_MISSING_IMG;
     public static Texture RELIC_CET4_IMG;
     public static Texture RELIC_CET6_IMG;
     public static Texture RELIC_N5_IMG;
+    public static Texture RELIC_N4_IMG;
+    public static Texture RELIC_N3_IMG;
+    public static Texture RELIC_N2_IMG;
+    public static Texture RELIC_N1_IMG;
     public static Texture RELIC_CET_OUTLINE;
     public static Texture POWER_CET_32;
     public static Texture POWER_CET_84;
@@ -54,9 +59,14 @@ public class ImageElements {
         }
         WORD_BUTTON_OUTLINE = ImageMaster.loadImage("CET46Resource/image/ui/button_word_outline.png");
 
+        RELIC_MISSING_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_missing.png");
         RELIC_CET4_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_cet4.png");
         RELIC_CET6_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_cet6.png");
         RELIC_N5_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_n5.png");
+        RELIC_N4_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_n4.png");
+        RELIC_N3_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_n3.png");
+        RELIC_N2_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_n2.png");
+        RELIC_N1_IMG = ImageMaster.loadImage("CET46Resource/image/relics/book_of_n1.png");
         RELIC_CET_OUTLINE = ImageMaster.loadImage("CET46Resource/image/relics/book_of_cet_outline.png");
 
         POWER_CET_32 = ImageMaster.loadImage("CET46Resource/image/powers/cet_power_32.png");
@@ -73,7 +83,7 @@ public class ImageElements {
             return (Texture) field.get(null);
         } catch (Exception e) {
             logger.info("No texture for lexicon: {}, err: {}", l == null ? "NULL" : l.name(), e);
-            return RELIC_CET4_IMG;
+            return RELIC_MISSING_IMG;
         }
     }
 
